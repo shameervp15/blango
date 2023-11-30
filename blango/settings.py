2a47041b6f2518a668cce72a676502fe5fa0e82e
Muhammed P <shameervp15@gmail.com>
2023-11-30 04:45:44 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -19,59 +19,60 @@ import dj_database_url
 
 
 class Dev(Configuration):
-  # Build paths inside the project like this: BASE_DIR / 'subdir'.
-  BASE_DIR = Path(__file__).resolve().parent.parent
+    # Build paths inside the project like this: BASE_DIR / 'subdir'.
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-  # Quick-start development settings - unsuitable for production
-  # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-  # SECURITY WARNING: keep the secret key used in production secret!
-  SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
 
-  # SECURITY WARNING: don't run with debug turned on in production!
-  # DEBUG = True
-  DEBUG = values.BooleanValue(True)
+    # SECURITY WARNING: don't run with debug turned on in production!
+    # DEBUG = True
+    DEBUG = values.BooleanValue(True)
 
-  # ALLOWED_HOSTS = ['*']
-  ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
+    # ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
 
-  X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
-  CSRF_COOKIE_SAMESITE = None
-  CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
-  CSRF_COOKIE_SECURE = True
-  SESSION_COOKIE_SECURE = True
-  CSRF_COOKIE_SAMESITE = 'None'
-  SESSION_COOKIE_SAMESITE = 'None'
+    X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
+    CSRF_COOKIE_SAMESITE = None
+    CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SAMESITE = 'None'
 
 
-  # Application definition
+    # Application definition
 
-  INSTALLED_APPS = [
-      'django.contrib.admin',
-      'django.contrib.auth',
-      'django.contrib.contenttypes',
-      'django.contrib.sessions',
-      'django.contrib.messages',
-      "django.contrib.sites",
-      'django.contrib.staticfiles',
+    INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    "django.contrib.sites",
+    'django.contrib.staticfiles',
 
-      'blango_auth',
-      'blog',
-      'crispy_forms',
-      'crispy_bootstrap5',
-      'debug_toolbar',
+    'blango_auth',
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'debug_toolbar',
 
-      "allauth",
-      "allauth.account",
-      "allauth.socialaccount",
-      "allauth.socialaccount.providers.google",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 
-      "rest_framework",
-      "rest_framework.authtoken",
-  ]
+    "rest_framework",
+    "rest_framework.authtoken",
+    "drf_yasg",
+    ]
 
-  MIDDLEWARE = [
+    MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,143 +81,143 @@ class Dev(Configuration):
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-  ]
+    ]
 
-  ROOT_URLCONF = 'blango.urls'
+    ROOT_URLCONF = 'blango.urls'
 
-  TEMPLATES = [
-      {
-          'BACKEND': 'django.template.backends.django.DjangoTemplates',
-          'DIRS': [BASE_DIR/'templates'],
-          'APP_DIRS': True,
-          'OPTIONS': {
-              'context_processors': [
-                  'django.template.context_processors.debug',
-                  'django.template.context_processors.request',
-                  'django.contrib.auth.context_processors.auth',
-                  'django.contrib.messages.context_processors.messages',
-              ],
-          },
-      },
-  ]
+    TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR/'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    ]
 
-  WSGI_APPLICATION = 'blango.wsgi.application'
-
-
-  # Database
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-  # DATABASES = {
-  #     'default': {
-  #         'ENGINE': 'django.db.backends.sqlite3',
-  #         'NAME': BASE_DIR / 'db.sqlite3',
-  #     }
-  # }
-  
-  DATABASES = {
-      "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"),
-      "alternative": dj_database_url.config(
-          "ALTERNATIVE_DATABASE_URL",
-          default=f"sqlite:///{BASE_DIR}/alternative_db.sqlite3",
-      ),
-  }
+    WSGI_APPLICATION = 'blango.wsgi.application'
 
 
-  # Password validation
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+    # Database
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-  AUTH_PASSWORD_VALIDATORS = [
-      {
-          'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-      },
-  ]
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
 
-
-  # Internationalization
-  # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-  LANGUAGE_CODE = 'en-us'
-
-  TIME_ZONE = values.Value("UTC")
-
-  USE_I18N = True
-
-  USE_L10N = True
-
-  USE_TZ = True
+    DATABASES = {
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"),
+    "alternative": dj_database_url.config(
+        "ALTERNATIVE_DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR}/alternative_db.sqlite3",
+    ),
+    }
 
 
-  # Static files (CSS, JavaScript, Images)
-  # https://docs.djangoproject.com/en/3.2/howto/static-files/
+    # Password validation
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-  STATIC_URL = '/static/'
+    AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    ]
 
-  # Default primary key field type
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-  DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    # Internationalization
+    # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-  #logging
-  LOGGING = {
-  "version": 1,
-  "disable_existing_loggers": False,
-  "formatters": {
-      "verbose": {
-          "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-          "style": "{",
-      },
-  },
-  "handlers": {
-      "console": {
-          "class": "logging.StreamHandler",
-          "stream": "ext://sys.stdout",
-          "formatter": "verbose",
-      },
-      #mailing the admins if any error occurred
-      # "mail_admins": {
-      #     "level": "ERROR",
-      #     "class": "django.utils.log.AdminEmailHandler",
-      #     "filters": ["require_debug_false"], prod config here
-      # },
-  },
-  "root": {
-      "handlers": ["console"],
-      "level": "DEBUG",
-  },
-  }
+    LANGUAGE_CODE = 'en-us'
 
-  #pip3 install django[argon2] 
-  PASSWORD_HASHERS = [
+    TIME_ZONE = values.Value("UTC")
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+    STATIC_URL = '/static/'
+
+    # Default primary key field type
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    #logging
+    LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+    "verbose": {
+        "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+        "style": "{",
+    },
+    },
+    "handlers": {
+    "console": {
+        "class": "logging.StreamHandler",
+        "stream": "ext://sys.stdout",
+        "formatter": "verbose",
+    },
+    #mailing the admins if any error occurred
+    # "mail_admins": {
+    #     "level": "ERROR",
+    #     "class": "django.utils.log.AdminEmailHandler",
+    #     "filters": ["require_debug_false"], prod config here
+    # },
+    },
+    "root": {
+    "handlers": ["console"],
+    "level": "DEBUG",
+    },
+    }
+
+    #pip3 install django[argon2] 
+    PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-  ]
+    ]
 
-  INTERNAL_IPS = ["192.168.11.179"]
+    INTERNAL_IPS = ["192.168.11.179"]
 
-  AUTH_USER_MODEL = "blango_auth.User"
+    AUTH_USER_MODEL = "blango_auth.User"
 
-  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-  ACCOUNT_ACTIVATION_DAYS = 7
-  # REGISTRATION_OPEN = False
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    # REGISTRATION_OPEN = False
 
-  SITE_ID = 1
-  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-  ACCOUNT_EMAIL_REQUIRED = True
-  ACCOUNT_USERNAME_REQUIRED = False
-  ACCOUNT_AUTHENTICATION_METHOD = "email"
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
-  REST_FRAMEWORK = {
+    REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
@@ -225,7 +226,15 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ]
-  }
+    }
+
+    # Swagger UI
+    SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+    "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+    "Basic": {"type": "basic"},
+    }
+    }
 
     
 
--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,17 +1,41 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+import os
 
 from blog.api_views import PostList, PostDetail, UserDetail
 
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+#swagger ui schema
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blango API",
+        default_version="v1",
+        description="API for Blango Blog",
+    ),
+    
+    public=True,
+)
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>/", PostDetail.as_view(), name="api_post_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", obtain_auth_token),
-    path("users/<str:email>/", UserDetail.as_view(), name="api_user_detail")
+    path("users/<str:email>/", UserDetail.as_view(), name="api_user_detail"),
+    re_path(
+        r"^swagger(\.json|\.yaml)$/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
